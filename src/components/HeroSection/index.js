2ea3405451f2f46f2e24c2a7f59921ae41737e71
XiwayBanh <xiway.banh@gmail.com>
2021-06-17 17:30:10 +0800

    added hero section component

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./HeroSection.css"
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      <h1>Hi,</h1>
+      <h2>I'm Xiway</h2>
+      <h2>A passionnate</h2>
+      <button>Contact me</button>
+    </div>
+  )
+}
+
+export default HeroSection
